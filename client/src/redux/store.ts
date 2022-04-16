72ca838cef19fb614fdffd502891aff166511455
sudharshan-eGov <sudarshan.k@egovernments.org>
2022-04-16 14:24:07 +0530

    .

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 import rootReducer from "./reducer";
 
 const appStore = createStore(
 	rootReducer,
-	composeWithDevTools(applyMiddleware())
+	composeWithDevTools(applyMiddleware(logger))
 );
 export default appStore;
