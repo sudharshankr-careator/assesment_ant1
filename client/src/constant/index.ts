2217b52af5634a3bbd9ec71ea7b89e3d3ed438d8
sudharshan-eGov <sudarshan.k@egovernments.org>
2022-04-16 14:10:01 +0530

    .

---
+++ client/src/constant/index.ts
@@ -0,0 +1,10 @@
+// const HOST = window.location.host;
+const HOST = "http://localhost:5000/graphql";
+const DATABASE_URL = HOST;
+const DATE_FORMAT = "DD/MM/YYYY";
+const CONSTANTS = {
+	HOST,
+	DATABASE_URL,
+	DATE_FORMAT,
+};
+export default CONSTANTS;
